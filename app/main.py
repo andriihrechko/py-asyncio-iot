6feaadc157c5a249d87f8c179f517734c585d6a2
andriihrechko <andriihrechko250703@gmail.com>
2026-06-22 22:41:47 +0300

    import type hints and remove unused time import

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+from typing import Awaitable, Any
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
